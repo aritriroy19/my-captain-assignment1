674d2bfd2d98b16007b1307c2314bfcfdf6ffe96
Aritri Roy <87139541+aritriroy19@users.noreply.github.com>
2021-07-12 22:41:44 +0530

    Add files via upload

---
+++ mycap.py
@@ -0,0 +1,11 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/ABHISHEK/Documents/Assignment.py
+10
+= RESTART: C:/Users/ABHISHEK/Documents/Assignment.py
+ radius of the circle : 10
+= RESTART: C:/Users/ABHISHEK/Documents/Assignment.py
+input the radius of the circle:5
+the area of the circle with radius5.0is:78.53981633974483
+>>> 
